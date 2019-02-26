03c1e326fe1c05cc6a95aa02e468683255f4e156
Alex <ghostfirelake@gmail.com>
2019-02-26 17:54:34 +0400

    simple

--- src/LarGen.php
+++ src/LarGen.php
@@ -2,24 +2,35 @@
 
 namespace Ra1ngo\LarGen;
 
-use PhpParserQuery\PQuery;
+use Ra1ngo\PhpParserQuery\PQuery;
+
+use Artisan;
+use Illuminate\Support\Str;
+use Illuminate\Support\Composer;
+use Illuminate\Database\Migrations\MigrationCreator;
 
 class LarGen
 {
     protected $pQuery;
+    protected $creator;
+    protected $composer;
 
-    public function __construct(PQuery $pQuery)
+    public function __construct(PQuery $pQuery, MigrationCreator $creator, Composer $composer)
     {
         $this->pQuery = $pQuery;
+        $this->creator = $creator;
+        $this->composer = $composer;
     }
 
-    public function genMigration($name, $fields, $foreignKeys)
+    public function genMigration($name, $fields = [], $foreignKeys = [])
     {
+        $file = $this->createMigration($name);
+        dd($file);
 
     }
 
     //создает модель и отношения у связанных моделей
-    public function genModelWithRelations($name, $fields, $relations)
+    public function genModelWithRelations($name, $fields = [], $relations = [])
     {
 
     }
@@ -28,17 +39,17 @@ class LarGen
     /////Поля/////
     //////////////
     //$name - имя миграции
-    public function addFieldMigration($name, $field, $args[])
+    public function addFieldMigration($name, $field, $args = [])
     {
 
     }
 
-    public function changeFieldMigration($name, $field, $args[])
+    public function changeFieldMigration($name, $field, $args = [])
     {
 
     }
 
-    public function removeFieldMigration($name, $field, $args[])
+    public function removeFieldMigration($name, $field, $args = [])
     {
 
     }
@@ -50,17 +61,17 @@ class LarGen
     ///////////////
     /////Ключи/////
     ///////////////
-    public function addForeignKey($name, $foreignKey, $args[])
+    public function addForeignKey($name, $foreignKey, $args = [])
     {
 
     }
 
-    public function changeForeignKey($name, $foreignKey, $args[])
+    public function changeForeignKey($name, $foreignKey, $args = [])
     {
 
     }
 
-    public function removeForeignKey($name, $foreignKey, $args[])
+    public function removeForeignKey($name, $foreignKey, $args = [])
     {
 
     }
@@ -72,17 +83,17 @@ class LarGen
     /////////////////////
     /////Поля модели/////
     /////////////////////
-    public function addFieldModel($name, $field, $args[])
+    public function addFieldModel($name, $field, $args = [])
     {
 
     }
 
-    public function changeFieldModel($name, $field, $args[])
+    public function changeFieldModel($name, $field, $args = [])
     {
 
     }
 
-    public function removeFieldModel($name, $field, $args[])
+    public function removeFieldModel($name, $field, $args = [])
     {
 
     }
@@ -96,17 +107,17 @@ class LarGen
     //////////////////////////
     /////Отношения модели/////
     //////////////////////////
-    public function addRelations($name, $field, $args[])
+    public function addRelations($name, $field, $args = [])
     {
 
     }
 
-    public function changeRelations($name, $field, $args[])
+    public function changeRelations($name, $field, $args = [])
     {
 
     }
 
-    public function removeRelations($name, $field, $args[])
+    public function removeRelations($name, $field, $args = [])
     {
 
     }
@@ -116,4 +127,22 @@ class LarGen
     //}
 
     //hasRelations() - если одно отношение правильное, второе нет - выкидывает ошибку
+
+
+
+    protected function createMigration($name)
+    {
+        $table = Str::plural(Str::snake(class_basename($name)));
+        $migration_name = "create_{$table}_table";
+        $create = true;
+        $migrationPath = database_path().DIRECTORY_SEPARATOR.'migrations';
+
+        $file = $this->creator->create(
+            $migration_name, $migrationPath, $table, $create
+        );
+
+        $this->composer->dumpAutoloads();
+
+        return $file;
+    }
 }
